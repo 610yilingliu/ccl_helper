1ea8103321bb5e23db9352b109bdd519c6d0056a
610yilingliu <yilingliu1994@gmail.com>
2024-11-09 18:15:55 +0800

    修改prompt；优化存储逻辑，创建本地文件夹方便人工复盘

--- application.py
+++ application.py
@@ -14,18 +14,21 @@ apikey = os.getenv("OPENAI_API_KEY")
 
 
 class ccl_helper:
-    def __init__(self, apikey):
+    def __init__(self, apikey, scoring_folder = 'scoring'):
         '''
         apikey为openai的apikey，存放在同目录.env文件中（请自行创建.env文件，内容为OPENAI_API_KEY = '你的key'，如果没有就去https://platform.openai.com/api-keys自己建一个(记得充钱)
         '''
         self.dialog = None
+        # 控制播放句子序号及储存路径
         self.dialogidx = 0
         self.voicepath = "voice"
         self.output = []
         self.client = OpenAI(
             api_key=apikey
         )
-
+        self.savefolder = os.path.join(scoring_folder, time.strftime("%Y-%m-%d-%H-%M", time.localtime()))
+        os.makedirs(self.savefolder, exist_ok=True)
+        
     def load_test(self, file):
         '''
         每行为一个对话轮次，常规情况是一行英文（考试时需要你说对应英文），一行中文（考试时需要你说对应英文）
@@ -93,13 +96,6 @@ class ccl_helper:
             out.write(response.content)
         print(f'MP3 Generated')
         return path
-    
-    def talk(self):
-        curtext = self.dialog[self.dialogidx]
-        soundpath = self.sound_generator(curtext)
-        self.playsound(soundpath)
-        self.dialogidx += 1
-        return curtext
 
     def record(self):
         '''
@@ -119,7 +115,7 @@ class ccl_helper:
         
         print("录音结束")
         audio_data = np.concatenate(audio_data, axis=0)
-        output_file = "tmp.wav"
+        output_file = os.path.join(self.savefolder, str(self.dialogidx) + '.wav')
         write(output_file, sample_rate, audio_data)
         print(f"录音已保存为 '{output_file}'")
 
@@ -128,7 +124,8 @@ class ccl_helper:
         '''
         调用openAI API语音转文字
         '''
-        audio_file = "tmp.wav"
+        # 读取reord种存储的wmv文件，存储是为了复盘
+        audio_file = os.path.join(self.savefolder, str(self.dialogidx) + '.wav')
         transcription = self.client.audio.transcriptions.create(
         model="whisper-1", 
         file=open(audio_file, "rb")
@@ -143,7 +140,7 @@ class ccl_helper:
         prompt = f"""
         原句：{sentence}
         考生翻译：{yourresponse}
-        请判断考生的翻译是否正确并给出建议
+        请判断考生的翻译是否正确并给出建议，在给出建议的时候不用再完整的提及原句和考生翻译，只需给出建议。如果考生翻译有错误之处，也需要给出正确的翻译
         """
         response = self.client.chat.completions.create(
 
@@ -159,20 +156,30 @@ class ccl_helper:
 
         return_message = f'原句：{sentence}, 你的回答：{yourresponse}, GPT评分：{response.choices[0].message.content}'
 
+
         return return_message
     
+    def write_gpt_result(self):
+        fname = os.path.join(self.savefolder, 'gpt_result.txt')
+        with open(fname, 'w') as f:
+            for line in self.output:
+                f.write(line + '\n\n')
+        print(f'GPT评分结果已保存为 {fname}')
+
     def test(self, test_path):
         self.load_test(test_path)
 
-        for sentence in self.dialog:
+        for _ in range(len(self.dialog)):
+            sentence = self.dialog[self.dialogidx]
             sentence_path = self.text_to_voice(sentence)
             self.playsound(sentence_path)
             self.record()
             recorded_text = self.voice_to_text()
-
             judgement = self.judgement(recorded_text, sentence)
             self.output.append(judgement)
-
+            # 下一句
+            self.dialogidx += 1
+        self.write_gpt_result()
 
     def play_sys_sound_only(self, test_path):
         self.load_test(test_path)
@@ -184,6 +191,7 @@ class ccl_helper:
 
 if __name__ == '__main__':
     ccl = ccl_helper(apikey)
-    ccl.play_sys_sound_only('./dialogs/02_booking_flight.txt')
-    # ccl.test('./dialogs/02_booking_flight.txt')
-    # print(ccl.output)
+    # ccl.play_sys_sound_only('./dialogs/03_group_tour')
+    ccl.test('./dialogs/03_group_tour')
+
+
